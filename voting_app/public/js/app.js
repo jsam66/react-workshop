6268afcf95edd4626ae08d5a27af9ebfdb1964ea
tr1m <gsonly.imo@gmail.com>
2020-12-15 12:04:38 +0300

    refactor: use property initializers

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,37 +1,63 @@
 class ProductList extends React.Component {
+    state = {
+        products: []
+    }
+
+    componentDidMount() {
+        this.setState({
+            products: Seed.products
+        })
+    }
+
+   handleProductUpVote = (id) => {
+     const nextProducts = this.state.products.map(product => {
+         if (product.id === id) {
+             return Object.assign({}, product, {
+                 votes: product.votes + 1
+             })
+         }  else {
+             return product
+         }
+     })
+     this.setState({
+         products: nextProducts
+     })
+   } 
+
     render() {
-        const products = Seed.products.sort((a,b) => b.votes -a.votes)
-        const productComponents = products.map(product => <Product key={product.id} id={product.id} title={product.title} description={product.description} url={product.url} votes={product.votes} submitterAvatarUrl={product.submitterAvatarUrl} productImageUrl={product.productImageUrl}/>)
+        const products = this.state.products.sort((a,b) => b.votes -a.votes)
+        const productComponents = products.map(product => <Product onVote={this.handleProductUpVote} key={product.id} id={product.id} title={product.title} description={product.description} url={product.url} votes={product.votes} submitterAvatarUrl={product.submitterAvatarUrl} productImageUrl={product.productImageUrl}/>)
         return <div className='ui unstackable items'>
             {productComponents}
         </div> 
     }
 }
 
-class Product extends React.Component {
-    render() {
-        return <div className='item'>
-            <div className='image'>
-                <img src={this.props.productImageUrl} />
-            </div>
-            <div className='middle aligned content'>
-                <div className='header'>
-                    <a>
-                        <i className='large caret up icon' />
-                        {this.props.votes}
-                    </a>
-                </div>
-                <div className='description'>
-                    <a href={this.props.url}>{this.props.title}</a>
-                    <p>{this.props.description}</p>
-                </div>
-                <div className='extra'>
-                    <span>Submitted by: </span>
-                    <img className='ui avatar image' src={this.props.submitterAvatarUrl}></img>
-                </div>
-            </div>
-        </div>  
+const Product  = (props) => {
+    const handleUpVote = () => {
+        props.onVote(props.id)
     }
+    return <div className='item'>
+        <div className='image'>
+            <img src={props.productImageUrl} />
+        </div>
+        <div className='middle aligned content'>
+            <div className='header'>
+                <a onClick={handleUpVote}>
+                    <i className='large caret up icon' />
+                    {props.votes}
+                </a>
+            </div>
+            <div className='description'>
+                <a href={props.url}>{props.title}</a>
+                <p>{props.description}</p>
+            </div>
+            <div className='extra'>
+                <span>Submitted by: </span>
+                <img className='ui avatar image' src={props.submitterAvatarUrl}></img>
+            </div>
+        </div>
+    </div>  
 }
 
 
